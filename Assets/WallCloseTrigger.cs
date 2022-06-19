70450d6501d72205f5894125ebf512bae6c025a4
wamballa <steve.tyrell@gmail.com>
2022-06-19 19:10:15 +0100

    added backroom

---
+++ Assets/WallCloseTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCloseTrigger : MonoBehaviour
+{
+
+    //public Transform wall;
+    //public AudioSource audio;
+
+    //public Ghost ghost;
+
+    //private void Start()
+    //{
+    //    audio.volume = 0;
+    //    ghost.speed = 0;
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+
+    //    print("HIT TRIGGER");
+    //    Vector3 pos = wall.localPosition;
+
+    //    pos.x = pos.x + 2.5f;
+
+    //    wall.localPosition = pos;
+    //    ghost.speed = 2f;
+
+    //    audio.volume = 0.3f;
+
+
+    //    Destroy(gameObject);
+    //}
+
+
+}
