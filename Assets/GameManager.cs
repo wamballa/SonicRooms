a4bfdbe18f21c8c2036ec49c45d000ae2e6862c4
wamballa <steve.tyrell@gmail.com>
2022-06-21 16:10:59 +0100

    version 1

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject[] itemsToVanish;
 
-    public AudioClip clip;
-    public AudioSource audioSource;
+    public GameObject sonicNew;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.Play();
+        //audioSource.Play();
+        sonicNew.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (GameObject go in itemsToVanish)
+            {
+                go.SetActive(false);
+                sonicNew.SetActive(true);
+            }
+        }
     }
 }
